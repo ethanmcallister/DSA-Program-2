aa2a5db022de7c59040f7c1e6373e78f7984069e
ethanmcallister <a02264644@aggies.usu.edu>
2024-09-22 12:07:58 -0600

    assn with 1st problem done

---
+++ src/Tree.java
@@ -0,0 +1,322 @@
+// ******************ERRORS********************************
+// Throws UnderflowException as appropriate
+
+import com.sun.source.tree.BinaryTree;
+
+class UnderflowException extends RuntimeException {
+    /**
+     * Construct this exception object.
+     *
+     * @param message the error message.
+     */
+    public UnderflowException(String message) {
+        super(message);
+    }
+}
+
+public class Tree {
+    private BinaryNode root;  // Root of tree
+    private String treeName;     // Name of tree
+
+    /**
+     * Create an empty tree
+     * @param label Name of tree
+     */
+    public Tree(String label) {
+        treeName = label;
+        root = null;
+    }
+
+    /**
+     * Create tree from list
+     * @param arr   List of elements
+     * @param label Name of tree
+     * @ordered true if we want an ordered tree
+     */
+    public Tree(Integer[] arr, String label, boolean ordered) {
+        treeName = label;
+        if (ordered) {
+            root = null;
+            for (int i = 0; i < arr.length; i++) {
+                bstInsert(arr[i]);
+            }
+        } else root = buildUnordered(arr, 0, arr.length - 1);
+    }
+
+
+    /**
+     * Build a NON BST tree by inorder
+     * @param arr nodes to be added
+     * @return new tree
+     */
+    private BinaryNode buildUnordered(Integer[] arr, int low, int high) {
+        if (low > high) return null;
+        int mid = (low + high) / 2;
+        BinaryNode curr = new BinaryNode(arr[mid], null, null);
+        curr.left = buildUnordered(arr, low, mid - 1);
+        curr.right = buildUnordered(arr, mid + 1, high);
+        return curr;
+    }
+
+
+    /**
+     * Change name of tree
+     * @param name new name of tree
+     */
+    public void changeName(String name) {
+        this.treeName = name;
+    }
+
+    /**
+     * Return a string displaying the tree contents as a single line
+     */
+    public String toString() {
+        if (root == null)
+            return treeName + " Empty tree";
+
+        return toString(root, 0);
+//            return treeName + " Please write the code to print me";
+    }
+
+    public String toString(BinaryNode root, int level) {
+        if (root == null) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+
+        // right tree
+        result.append(toString(root.right, level + 1));
+
+        // add indentation (4 per level)
+        for (int i = 0; i < level; i++) {
+            result.append("    ");
+        }
+        result.append(root.element).append("\n");
+
+        // left tree
+        result.append(toString(root.left, level + 1));
+        return result.toString();
+    }
+    /**
+     * Return a string displaying the tree contents as a single line
+     */
+    public String toString2() {
+        if (root == null)
+            return treeName + " Empty tree";
+        else
+            return treeName + " " + toString2(root);
+    }
+
+    /**
+     * Internal method to return a string of items in the tree in order
+     * This routine runs in O(??)
+     *
+     * @param t the node that roots the subtree.
+     */
+    public String toString2(BinaryNode t) {
+        if (t == null) return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(toString2(t.left));
+        sb.append(t.element.toString() + " ");
+        sb.append(toString2(t.right));
+        return sb.toString();
+    }
+
+
+    /**
+     * The complexity of finding the deepest node is O(???)
+     * @return
+     */
+    public Integer deepestNode() {
+        return -1;
+    }
+
+    /**
+     * The complexity of finding the flip is O(???)
+     * reverse left and right children recursively
+     */
+    public void flip() {
+        flip(root);
+    }
+    private void flip(BinaryNode t){
+
+    }
+
+    /**
+     * Counts number of nodes in specified level
+     * The complexity of nodesInLevel is O(???)
+     * @param level Level in tree, root is zero
+     * @return count of number of nodes at specified level
+     */
+    public int nodesInLevel(int level) {
+        System.out.println("nodesInLevel needs to return the nodes at " + level);
+        return 0;
+    }
+
+    /**
+     * Print all paths from root to leaves
+     * The complexity of printAllPaths is O(???)
+     */
+    public void printAllPaths() {
+        System.out.println("printAllPaths does nothing");
+    }
+
+
+    /**
+     * Counts all non-null binary search trees embedded in tree
+     *  The complexity of countBST is O(???)
+     * @return Count of embedded binary search trees
+     */
+    public Integer countBST() {
+        if (root == null) return 0;
+        return -1;
+    }
+
+    /**
+     * Insert into a bst tree; duplicates are allowed
+     * The complexity of bstInsert depends on the tree.  If it is balanced the complexity is O(log n)
+     * @param x the item to insert.
+     */
+    public void bstInsert(Integer x) {
+
+        root = bstInsert(x, root);
+    }
+
+    /**
+     * Internal method to insert into a subtree.
+     * In tree is balanced, this routine runs in O(log n)
+     * @param x the item to insert.
+     * @param t the node that roots the subtree.
+     * @return the new root of the subtree.
+     */
+    private BinaryNode bstInsert(Integer x, BinaryNode t) {
+        if (t == null)
+            return new BinaryNode (x, null, null);
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = bstInsert(x, t.left);
+        } else {
+            t.right = bstInsert(x, t.right);
+        }
+        return t;
+    }
+
+    /**
+     * Determines if item is in tree
+     * @param item the item to search for.
+     * @return true if found.
+     */
+    public boolean contains(Integer item) {
+        return contains(item, root);
+    }
+
+    /**
+     * Internal method to find an item in a subtree.
+     * This routine runs in O(log n) as there is only one recursive call that is executed and the work
+     * associated with a single call is independent of the size of the tree: a=1, b=2, k=0
+     *
+     * @param x is item to search for.
+     * @param t the node that roots the subtree.
+     * @return node containing the matched item.
+     */
+    private boolean contains(Integer x, BinaryNode t) {
+        if (t == null)
+            return false;
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            return contains(x, t.left);
+        else if (compareResult > 0)
+            return contains(x, t.right);
+        else {
+            return true;    // Match
+        }
+    }
+    /**
+     * Remove all paths from tree that sum to less than given value
+     * @param sum: minimum path sum allowed in final tree
+     */
+    public void pruneK(Integer sum) {
+    }
+
+    /**
+     * Build tree given inOrder and preOrder traversals.  Each value is unique
+     * @param inOrder  List of tree nodes in inorder
+     * @param preOrder List of tree nodes in preorder
+     */
+    public void buildTreeFromTraversals(Integer[] inOrder, Integer[] preOrder) {
+        root = null;
+    }
+
+    /**
+     * Find the least common ancestor of two nodes
+     * @param a first node
+     * @param b second node
+     * @return String representation of ancestor
+     */
+    public BinaryNode lca(BinaryNode  t,Integer a, Integer b) {
+        return t;
+
+    }
+    public Integer sumAll(){
+        BinaryNode  r =   root;
+        return sumAll(r);
+    }
+    public Integer sumAll(BinaryNode  t){
+        if (t==null) return 0;
+        return t.element + sumAll(t.left) + sumAll(t.right);
+    }
+
+    public Integer lca(Integer a, Integer b) {
+
+         BinaryNode  l  = lca(root,a,b);
+         if (l==null) return null;
+         return l.element;
+
+    }
+    /**
+     * Balance the tree
+     */
+    public void balanceTree() {
+        //root = balanceTree(root);
+    }
+
+    /**
+     * In a BST, keep only nodes between range
+     *
+     * @param a lowest value
+     * @param b highest value
+     */
+    public void keepRange(Integer a, Integer b) {
+    }
+
+    // Basic node stored in unbalanced binary  trees
+    public static class BinaryNode  {
+        Integer element;            // The data in the node
+        BinaryNode left;   // Left child
+        BinaryNode  right;  // Right child
+
+        // Constructors
+        BinaryNode(Integer theElement) {
+            this(theElement, null, null);
+        }
+
+        BinaryNode(Integer theElement, BinaryNode lt, BinaryNode rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+        }
+
+        // toString for BinaryNode
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Node:");
+            sb.append(element);
+            return sb.toString();
+        }
+
+    }
+
+
+}
